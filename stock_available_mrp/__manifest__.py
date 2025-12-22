rousseldenis/stock-logistics-warehouse

[10.0][MIG] port stock_available_mrp to 10.0

stock_available_mrp/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Consider the production potential is available to promise',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     "author": u"Numérigraphe,"
               u"Odoo Community Association (OCA)",
     'category': 'Hidden',
@@ -15,8 +15,8 @@
         'views/product_template_view.xml',
     ],
     'demo': [
-        'demo/mrp_bom.yml',
+        'data/mrp_data.xml',
     ],
     'license': 'AGPL-3',
-    'installable': False,
+    'installable': True,
 }

StefanRijnhart/stock-logistics-availability

stock_available_mrp/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Consider the production potential is available to promise',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     "author": u"Numérigraphe,"
               u"Odoo Community Association (OCA)",
     'category': 'Hidden',
@@ -15,8 +15,8 @@
         'views/product_template_view.xml',
     ],
     'demo': [
-        'demo/mrp_bom.yml',
+        'demo/mrp_data.xml',
     ],
     'license': 'AGPL-3',
-    'installable': False,
+    'installable': True,
 }